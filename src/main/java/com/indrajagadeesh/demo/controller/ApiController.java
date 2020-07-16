4c7834861b8e8623d4ed24982a051b480851125b
indrajagadeesh <indrajagadeesh121@gmail.com>
2020-07-16 06:24:01 +0530

    logging

--- src/main/java/com/indrajagadeesh/demo/controller/ApiController.java
+++ src/main/java/com/indrajagadeesh/demo/controller/ApiController.java
@@ -1,19 +1,22 @@
 package com.indrajagadeesh.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class ApiController {
 
     @GetMapping("/")
     public String index() {
+        log.info("hot successful");
         return "<h1> Hello Medium ....!</h1><br>";
     }
 
     @GetMapping("/{name}")
     public String hello(@PathVariable String name) {
+        log.info("hot successful and message is {}",name);
         return "<h2> Hello "+name+" ....!</h2>";
     }
 }
